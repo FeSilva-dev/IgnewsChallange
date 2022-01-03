6276df467bbe1166e668c622e8b123d0045b4191
FeSilva-dev <feliper.silva011@gmail.com>
2022-01-03 03:40:17 -0300

    start ignews blog challange

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import commonStyles from '../../styles/common.module.scss';
+import { useRouter } from 'next/router';
+
 export default function Header() {
-  // TODO
+
+  const router = useRouter()
+  
+  return(
+    <header className={commonStyles.container}>
+      <img src="/logo.svg" alt="logo" onClick={() => router.push('/', '', {})} />
+    </header>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head  from 'next/head';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,58 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return(
+    <>
+      <Head>
+        <title>spacetraveling</title>
+      </Head>
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          <a href="">
+            <strong>Titulo do meu post</strong>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+              Quo accusantium quibusdam explicabo officia, odio dicta corrupti magnam, 
+            </p>
+            <div className={styles.postInfo}>
+              <time>8 de Dezembro de 2021</time>
+              <span>Felipe Teste</span>
+            </div>
+          </a>
+
+          <a href="">
+            <strong>Titulo do meu post</strong>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+              Quo accusantium quibusdam explicabo officia, odio dicta corrupti magnam, 
+            </p>
+            <div className={styles.postInfo}>
+              <time>8 de Dezembro de 2021</time>
+              <span>Felipe Teste</span>
+            </div>
+          </a>
+
+          <a href="">
+            <strong>Titulo do meu post</strong>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+              Quo accusantium quibusdam explicabo officia, odio dicta corrupti magnam, 
+            </p>
+            <div className={styles.postInfo}>
+              <time>8 de Dezembro de 2021</time>
+              <span>Felipe Teste</span>
+            </div>
+          </a>
+
+          <div className={styles.loadMore}>
+            <a href="">Carregar mais posts</a>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
